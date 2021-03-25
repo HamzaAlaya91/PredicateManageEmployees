b487711ba9d9f75f9bcd01777b8802bd15076029
Paccino <alaya.hamza1@gmail.com>
2021-03-25 16:09:14 +0100

    Update PredicateExemple.java

--- src/packageTest/PredicateExemple.java
+++ src/packageTest/PredicateExemple.java
@@ -13,18 +13,10 @@ public class PredicateExemple {
 	
 	
 	Predicate<String> str = s -> s.startsWith("a"); // creating predicate
-	
-	
-	
-	
-	
-	
-	
 
 	public static void main(String[] args) {
 		
-		
-		
+
 		PredicateExemple predicateClass = new PredicateExemple();
 		
 		System.out.println(predicateClass.p.test(5)); // calling predicate function
@@ -33,10 +25,9 @@ public class PredicateExemple {
 		System.out.println(predicateClass.str.test("sami")); // calling predicate function
 		
 		
-		
 		/* 
 		Exemple tableau
-		filtre dans un nouveau tableau les entiers superieur à 10
+		filtre dans un nouveau tableau les entiers superieur Ã  10
 		 */
 		
 		List<Integer> list = Arrays.asList(1,5,8,12,4,36,47,87,5,6);
